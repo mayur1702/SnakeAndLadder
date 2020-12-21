d802987bbc9bbf95b86c779514d92f3b46604a4f
mayur <mayur.smistry17021995@gmail.com>
2020-12-21 19:38:06 +0530

    ws basic connection

--- app.js
+++ app.js
@@ -1,41 +1,22 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const port = 5000;
+const server = http.createServer(express);
+const wss = new WebSocket.Server({server});
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+wss.on("connection", (connection) => {  
+  connection.send("Connected!");
+  connection.on("message", (data) => {
+    wss.clients.forEach(client => {
+      if(client != connection && client.readyState == WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  })
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+})
