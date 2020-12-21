830ebc978ea65523abc79feb8b79caace55a3f75
mayur <mayur.smistry17021995@gmail.com>
2020-12-21 16:00:03 +0530

    component

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="main-container">
+      <div className="grid"></div>
+      <div className="dice-container"></div>
+    </div>
+  );
+}
+
+export default App;
