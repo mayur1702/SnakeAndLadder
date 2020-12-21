cca1fb0a3dae57cb3e4e5c64a6cdd6341d78cc72
tejas.nas <tejas.nas>
2020-12-21 17:08:36 +0530

    board

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.scss';
+import Board from './Components/Board';
 
 function App() {
-  return (
-    <div className="main-container">
-      <div className="grid"></div>
-      <div className="dice-container"></div>
-    </div>
-  );
-}
-
+    return (
+        <div className="main-container">
+        <div className="board"><Board /></div>
+        <div className="dice-container"></div>
+        </div>
+    );
+}  
 export default App;
